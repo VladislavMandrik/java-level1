b0462f184e78bd382263765debe4d20deeea19c0
VladislavMandrik <ladvdrikman@gmail.com>
2022-04-17 13:28:57 +0300

    changed first task, added 2-4 task

--- src/main/java/ru/gb/mandrik/Main.java
+++ src/main/java/ru/gb/mandrik/Main.java
@@ -3,14 +3,39 @@ package ru.gb.mandrik;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println(checkNumbers(-2, 6));
+        System.out.println(checkNumbers(5, 6));
+        System.out.println(compareNumber(6));
+        System.out.println(compareNumberWithBoolean(-3));
+        print("Hello", 8);
     }
 
-    public static String checkNumbers(int a, int b) {
+    public static boolean checkNumbers(int a, int b) {
         if (a + b > 10 && a + b <= 20) {
-            return "true";
+            return true;
         } else {
-            return "false";
+            return false;
+        }
+    }
+
+    public static String compareNumber(int a) {
+        if (a >= 0) {
+            return "Положительное";
+        } else {
+            return "Отрицательное";
+        }
+    }
+
+    public static boolean compareNumberWithBoolean(int a) {
+        if (a >= 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void print(String a, int b) {
+        for (int i = 0; i < b; i++) {
+            System.out.println(a);
         }
     }
 }
