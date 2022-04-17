fc0a061b57700955c3e9790c4ae4232717fa8300
VladislavMandrik <ladvdrikman@gmail.com>
2022-04-17 12:23:07 +0300

    added first task

--- src/main/java/ru/gb/mandrik/Main.java
+++ src/main/java/ru/gb/mandrik/Main.java
@@ -1,4 +1,16 @@
 package ru.gb.mandrik;
 
 public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(checkNumbers(-2, 6));
+    }
+
+    public static String checkNumbers(int a, int b) {
+        if (a + b > 10 && a + b <= 20) {
+            return "true";
+        } else {
+            return "false";
+        }
+    }
 }
